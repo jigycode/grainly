7ed5e033996c47641124ce63187fed12a509a462
jigycode <jigyasha040106@gmail.com>
2024-12-30 15:38:12 +0530

    implement routing and add login page; create Navbar component

---
+++ src/assets/component/Home.jsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <a href="/">
+          <li>Home</li>
+        </a>
+        <a href="/">
+          <li></li>about
+        </a>
+        <a href="/">
+          <li>contact</li>
+        </a>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
